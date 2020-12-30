51f4b213369ea5d28b637a32b8d00fe7433de750
Rahul Agarwal <rahulagarwal0605@gmail.com>
2020-12-31 05:07:05 +0530

    up

--- app/main.py
+++ app/main.py
@@ -13,11 +13,10 @@ def index():
     # chrome_options.add_argument('--headless')
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
     driver = webdriver.Chrome(chrome_options=chrome_options)
-    # driver.implicitly_wait(10)
+    driver.implicitly_wait(10)
     driver.get("https://web.whatsapp.com")
-    return driver.page_source
-    # element = None
-    # while element==None:
-    #     element = driver.find_element_by_class_name('_1yHR2').get_attribute("data-ref")
-    # app.logger.info(element)
-    # return render_template('index.html', data = qrcode(element, box_size=4, error_correction='H'))
+    element = None
+    while element==None:
+        element = driver.find_element_by_class_name('_1yHR2').get_attribute("data-ref")
+    app.logger.info(element)
+    return render_template('index.html', data = qrcode(element, box_size=4, error_correction='H'))
