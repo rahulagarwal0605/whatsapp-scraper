fc642ffbe2770080809f4a06d79a668e00cb937f
Rahul Agarwal <rahulagarwal0605@gmail.com>
2020-12-31 02:48:10 +0530

    up

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ qrcode = QRcode(app)
 @app.route('/index')
 def index():
     driver = webdriver.Chrome()
-    driver.implicitly_wait(100)
+    driver.implicitly_wait(10)
     driver.get("https://web.whatsapp.com")
     element = None
-    while element==None:
-        element = driver.find_element_by_class_name('_1yHR2').get_attribute("data-ref")
+    # while element==None:
+    #     element = driver.find_element_by_class_name('_1yHR2').get_attribute("data-ref")
     app.logger.info(element)
     return render_template('index.html', data = qrcode(element, box_size=4, error_correction='H'))
